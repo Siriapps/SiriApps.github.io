df705a359ce562cfd9007461a61f8bc85cc2a76e
Siri Appalaneni <siriapps3@gmail.com>
2025-11-25 16:27:28 -0600

    fresh start

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // This ensures assets load correctly on the root domain
-})
-
